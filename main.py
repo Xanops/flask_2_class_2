a724b1322917014bbb42d2c5f8d09384ab2326a8
Xanops <pzvpetr@gmail.com>
2021-03-16 17:31:54 +0300

    second release

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('page.html', title=title)
+
+
 @app.route('/training/<prof>')
 def training(prof):
     if 'инженер' in prof.lower():
-        return render_template('page.html', arg='Инженерные тренажеры')
+        return render_template('train.html', arg='Инженерные тренажеры')
+    elif 'строител' in prof.lower():
+        return render_template('train.html', arg='Научные симуляторы')
     else:
-        return render_template('page.html', arg='Научные симуляторы')
+        return render_template('train.html', arg='Базовый тренажёр')
 
 
 if __name__ == '__main__':
